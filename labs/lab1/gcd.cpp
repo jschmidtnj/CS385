b648784ae440b87deadb72627021af6a7ee82e11
Joshua <jschmidtnj@gmail.com>
2019-08-29 11:18:18 -0400

    gcd: finished gcd lab
    finished gcd lab using simple algorithm for recursive and temp
    
    Signed-off-by: Joshua <jschmidtnj@gmail.com>

--- labs/lab1/gcd.cpp
+++ labs/lab1/gcd.cpp
@@ -1,8 +1,18 @@
+/*******************************************************************************
+ * Name    : gcd.cpp
+ * Author  : Joshua Schmidt
+ * Version : 1.0
+ * Date    : August 29, 2019
+ * Description : Computes the GCD of two command-line arguments.
+ * Pledge : I pledge my honor that I have abided by the Stevens Honor System.
+ ******************************************************************************/
+
 #include <iostream>
 #include <sstream>
 
 using namespace std;
 
+// do the same algorithm as recursive function but iterative
 int gcd_iterative(int m, int n) {
   int temp;
   while (m != 0) {
@@ -10,15 +20,20 @@ int gcd_iterative(int m, int n) {
     m = n % m;
     n = temp;
   }
-  return n < 0 ? -n : n;
+  return abs(n);
 }
 
+// algorithm is to take mod of n and m, and keep passing
+// in m as n to keep modding by remainder
+// make sure it's a positive number
 int gcd_recursive(int m, int n) {
   if (m == 0)
-    return n < 0 ? -n : n;
+    return abs(n);
   return gcd_recursive(n % m, m);
 }
 
+// use same code for number input
+// then make sure output is the same
 int main(int argc, char* const argv[]) {
   int m, n;
   istringstream iss;
