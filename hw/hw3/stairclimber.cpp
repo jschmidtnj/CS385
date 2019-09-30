68defda5c2c6273f1b7200066d86f8abfbf15684
Joshua <jschmidtnj@gmail.com>
2019-09-30 11:17:14 -0400

    removed the sorting to make the algorithm much more efficient

--- hw/hw3/stairclimber.cpp
+++ hw/hw3/stairclimber.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Name        : staircase.cpp
+ * Name        : stairclimber.cpp
  * Author      : Joshua Schmidt
  * Version     : 1.0
  * Date        : 9/23/2019
@@ -14,86 +14,24 @@
 using namespace std;
 
 /**
- * swaps two int vectors
- */
-void swap(vector<int>& x, vector<int>& y) {
-  vector<int> temp = x;
-  x = y;
-  y = temp;
-}
-
-/**
- * sorts the nested vector by column
- */
-void sort_by_column(vector<vector<int>> &arr, const int &num_stairs)
-{
-  for (int column = 0; column < num_stairs; column++)
-    for (long unsigned int i = 0; i < arr.size(); i++) {
-      int swap_index = i;
-      if ((int)arr[i].size() >= (column + 1))
-        for (long unsigned int j = i + 1; j < arr.size(); j++)
-          if ((int)arr[j].size() >= (column + 1) && arr[j][column] < arr[swap_index][column]) {
-            if (column == 0) {
-              swap_index = j;
-            } else {
-              bool swap_vals = true;
-              for (int k = column - 1; k >= 0; k--) {
-                if (arr[j][k] != arr[swap_index][k]) {
-                  swap_vals = false;
-                  break;
-                }
-              }
-              if (swap_vals)
-                swap_index = j;
-            }
-          }
-      if (swap_index != (int)i)
-        swap(arr[swap_index], arr[i]);
-    }
-}
-
-/**
  * algorithm to get all the ways to climb the stairs
  */
-vector<vector<int>> _get_ways(const int num_stairs, unordered_map<int, vector<vector<int>>>& store)
+vector<vector<int>> get_ways(const int num_stairs)
 {
-  // if (store.count(num_stairs) > 0)
-  //   return store[num_stairs];
   vector<vector<int>> ways;
   if (num_stairs <= 0) {
     ways.push_back(vector<int>());
   } else {
     for (int i = 1; i <= 3; i++) {
       if (num_stairs >= i) {
-        vector<vector<int>> result = _get_ways(num_stairs - i, store);
+        vector<vector<int>> result = get_ways(num_stairs - i);
         for (vector<int>& res : result)
           res.insert(res.begin(), i);
-        // store[num_stairs - i] = result;
         ways.insert(ways.end(), result.begin(), result.end());
       }
     }
   }
   return ways;
-}
-
-/**
- * helper function to pass in store for dynamic algorithm
- */
-vector<vector<int>> get_ways(const int &num_stairs)
-{
-  unordered_map<int, vector<vector<int>>> store;
-  vector<vector<int>> res = _get_ways(num_stairs, store);
-  /*
-  for (int i = 0; i < 5; i++) {
-    for (vector<int> res1 : store[i]) {
-      for (int res2 : res1) {
-        cout << res2 << ' ';
-      }
-      cout << '\n';
-    }
-  }
-  */
-  return res;
 }
 
 /**
@@ -150,6 +88,5 @@ int main(int argc, char *const argv[])
     return 1;
   }
   vector<vector<int>> staircase_ways = get_ways(num_stairs);
-  sort_by_column(staircase_ways, num_stairs);
   display_ways(staircase_ways, num_stairs);
 }
