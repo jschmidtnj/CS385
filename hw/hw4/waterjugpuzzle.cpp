5d150a88b1091e334ae3c9c0bc600082f608d9be
Joshua <jschmidtnj@gmail.com>
2019-10-03 11:59:54 -0400

    added lab6 and finished most of waterjug. needs to be tested still

--- hw/hw4/waterjugpuzzle.cpp
+++ hw/hw4/waterjugpuzzle.cpp
@@ -9,26 +9,127 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
+#include <queue>
+#include <stack>
 
 using namespace std;
+
+struct State
+{
+  State(vector<int> levels, State *parent) : levels(levels), parent(parent){};
+  vector<int> levels;
+  State *parent;
+};
 
 class WaterJugs
 {
 public:
-  WaterJugs(vector<int> capacities, vector<int> goals)
-      : _capacities(capacities), _goals(goals){};
+  WaterJugs(vector<int> capacities, vector<int> goals);
+  void print_solution();
 
 private:
-  vector<int> _capacities, _goals, _state;
-  void print_initial_state();
+  vector<int> _capacities, _goals;
+  vector<vector<State>> _complete_state;
+  State * _final_state = NULL;
+  void find_solution();
 };
 
-void WaterJugs::print_initial_state()
+WaterJugs::WaterJugs(vector<int> capacities, vector<int> goals) : _capacities(capacities), _goals(goals)
 {
+  find_solution();
+}
+
+void WaterJugs::print_solution()
+{
+  if (_final_state == NULL)
+  {
+    cout << "No solution." << endl;
+    return;
+  }
   cout << "Initial state. (";
   for (long unsigned int i = 0; i < _capacities.size() - 1; i++)
     cout << "0, ";
   cout << _capacities.back() << ')' << endl;
+  stack<State *> steps;
+  for (State *current = _final_state; current->levels != _complete_state[0][0].levels; current = current->parent)
+  {
+    steps.push(current);
+  }
+  State *last = &_complete_state[0][0];
+  for (State *current = steps.top(); steps.size() > 0; steps.pop())
+  {
+    int from_index = -1, to_index = -1, amount_poured;
+    for (long unsigned int i = 0; i < _capacities.size() - 1; i++)
+    {
+      if (last->levels[i] != current->levels[i])
+      {
+        if (last->levels[i] < current->levels[i])
+        {
+          from_index = i;
+          amount_poured = current->levels[i] - last->levels[i];
+          if (to_index != -1)
+            break;
+        }
+        else
+        {
+          to_index = i;
+          if (from_index != -1)
+            break;
+        }
+      }
+    }
+    cout << "Pour " << amount_poured << " gallon" << (amount_poured != 1 ? "s" : "")
+         << " from " << char(from_index + 66) << " to " << char(to_index + 66) << ". (";
+    cout << current->levels[0];
+    for (long unsigned int i = 1; i < _capacities.size(); i++)
+      cout << ", " << current->levels[i];
+    cout << ")\n";
+    last = current;
+  }
+}
+
+void WaterJugs::find_solution()
+{
+  _complete_state.resize(_capacities[2] + 1);
+  for (vector<State> &second_state : _complete_state)
+    _complete_state.resize(_capacities[2] + 1);
+  queue<State> _mainqueue;
+  _mainqueue.push(State(vector<int>{0, 0, _capacities.back()}, NULL));
+  State *current = NULL;
+  while (_mainqueue.size() > 0)
+  {
+    current = &_mainqueue.front();
+    if (!_complete_state[current->levels[0]][current->levels[1]].levels.empty())
+    {
+      _mainqueue.pop();
+      continue;
+    }
+    _complete_state[current->levels[0]][current->levels[1]] = *current;
+    if (current->levels == _goals)
+    {
+      _mainqueue.pop();
+      break;
+    }
+    // c->a, c->b, b->a, b->c, a->b, a->c
+    for (int i = 0; i < current->levels.size(); i++)
+    {
+      for (int j = 0; j < current->levels.size(); j++)
+      {
+        if (i != j && current->levels[i] != 0 && current->levels[j] < _capacities[j])
+        {
+          int remaining_space = _capacities[j] - current->levels[j];
+          int amount_poured = remaining_space - current->levels[i] > 0 ? current->levels[i] : remaining_space;
+          vector<int> new_levels = current->levels;
+          new_levels.reserve(current->levels.size());
+          new_levels[i] = current->levels[i] - amount_poured;
+          new_levels[j] = current->levels[j] + amount_poured;
+          _mainqueue.push(State(new_levels, &_complete_state[current->levels[i]][current->levels[j]]));
+        }
+      }
+    }
+    _mainqueue.pop();
+  }
+  _final_state = current;
 }
 
 int main(int argc, const char *argv[])
@@ -82,4 +183,5 @@ int main(int argc, const char *argv[])
     return -1;
   }
   WaterJugs jugs(capacities, goals);
+  jugs.print_solution();
 }
