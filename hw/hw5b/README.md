64640c3dd5162b5793172b5651cb3afb94312192
Joshua <jschmidtnj@gmail.com>
2019-11-03 17:45:52 -0500

    added anagram finder

---
+++ hw/hw5b/README.md
@@ -0,0 +1,7 @@
+# anagram finder
+
+- takes in a dictionary of words, and finds the anagrams
+- one approach uses prime numbers, assigning each number to a prime number and then sorting based off of that - should be O(n)
+- this works for up to 9 characters with unsigned long long
+- another is to just use a dictionary for the sorted letters
+- this works for more characters so it's the best way to go
---
+++ hw/hw5b/anagramfinder.cpp
@@ -0,0 +1,205 @@
+/*******************************************************************************
+ * Name        : anagramfinder.cpp
+ * Author      : Joshua Schmidt
+ * Version     : 1.0
+ * Date        : 11/3/2019
+ * Description : Find all the anagrams.
+ * Pledge      : I pledge my honor that I have abided by the Stevens Honor System.
+ ******************************************************************************/
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <unordered_map>
+#include <vector>
+#include <sstream>
+#include <bits/stdc++.h>
+#include <random>
+
+/**
+ * class for finding anagrams
+ */
+class FindAnagrams
+{
+public:
+  explicit FindAnagrams(std::string filename);
+  void printAnagrams();
+
+private:
+  std::string getSortedKey(std::string word);
+  std::string dictionaryFilename;
+  std::vector<std::string> mostAnagramKeys;
+  unsigned long mostAnagrams = 0;
+  std::default_random_engine generator;
+  std::unordered_map<std::string, std::vector<std::string>> words;
+  void swapStrings(std::string &elem1, std::string &elem2);
+  long partitionKeys(std::string keys[], long left, long right);
+  void quickSortKeys(std::string keys[], long left, long right);
+};
+
+/**
+ * printAnagrams
+ * 
+ * output the anagrams for given object
+ */
+void FindAnagrams::printAnagrams()
+{
+  if (mostAnagrams == 0)
+  {
+    std::cout << "No anagrams found.\n";
+    return;
+  }
+  std::cout << "Max anagrams: " << mostAnagrams;
+  // first sort the elements in the keys
+  for (unsigned long i = 0; i < mostAnagramKeys.size(); i++)
+    std::sort(words[mostAnagramKeys[i]].begin(),
+              words[mostAnagramKeys[i]].end(),
+              std::less<std::string>());
+  // then sort based on first element for each key
+  if (mostAnagramKeys.size() > 0)
+    quickSortKeys(&mostAnagramKeys[0], 0, mostAnagramKeys.size() - 1);
+  for (unsigned long i = 0, j; i < mostAnagramKeys.size(); i++)
+  {
+    std::cout << '\n';
+    for (j = 0; j < mostAnagrams; j++)
+      std::cout << words[mostAnagramKeys[i]][j] << '\n';
+  }
+}
+
+/**
+ * swapStrings
+ * 
+ * swap strings in array
+ */
+void FindAnagrams::swapStrings(std::string &elem1, std::string &elem2)
+{
+  std::string temp = elem1;
+  elem1 = elem2;
+  elem2 = temp;
+}
+
+/**
+ * partitionKeys
+ * 
+ * partitions keys using Lomuto with
+ * random partition index
+ */
+long FindAnagrams::partitionKeys(std::string keys[], long left, long right)
+{
+  std::uniform_int_distribution<long> dist(left, right);
+  unsigned long randIndex = dist(generator);
+  swapStrings(keys[randIndex], keys[left]);
+  std::string pivot = words[keys[left]][0];
+  long s = left;
+  for (long i = left + 1; i <= right; i++)
+    if (words[keys[i]][0] < pivot)
+      swapStrings(keys[++s], keys[i]);
+  swapStrings(keys[left], keys[s]);
+  return s;
+}
+
+/**
+ * quickSortKeys
+ * 
+ * quick sort the array based on first element in words
+ */
+void FindAnagrams::quickSortKeys(std::string keys[], long left, long right)
+{
+  if (left < right)
+  {
+    long partition = partitionKeys(keys, left, right);
+    quickSortKeys(keys, left, partition - 1);
+    quickSortKeys(keys, partition + 1, right);
+  }
+}
+
+/**
+ * getSortedKey
+ * 
+ * gets the sorted key for a given word
+ */
+std::string FindAnagrams::getSortedKey(std::string word)
+{
+  int charCount[26] = {0};
+  for (int i = 0; i < (int)word.length(); i++)
+  {
+    // ignore spaces
+    if (word[i] == ' ')
+      continue;
+    if (word[i] >= 'a' && word[i] <= 'z')
+      charCount[word[i] - 'a']++;
+    else if (word[i] >= 'A' && word[i] <= 'Z')
+      charCount[word[i] - 'A']++;
+    else
+      throw std::invalid_argument("invalid character found");
+  }
+  std::stringstream ss;
+  for (int i = 0; i < 26; i++)
+    for (int j = 0; j < charCount[i]; j++)
+      ss << (char)('a' + i);
+  return ss.str();
+}
+
+/**
+ * FindAnagrams constructor
+ * 
+ * creates FinAnagram object from
+ * filename of dictionary
+ */
+FindAnagrams::FindAnagrams(std::string filename)
+{
+  dictionaryFilename = filename;
+  std::ifstream dictionaryFile(dictionaryFilename);
+  std::string word;
+  while (std::getline(dictionaryFile, word))
+  {
+    // std::cout << line << std::endl;
+    std::string key = getSortedKey(word);
+    words[key].push_back(word);
+    if (words[key].size() < mostAnagrams)
+      continue;
+    else if (words[key].size() > mostAnagrams && words[key].size() > 1)
+    {
+      mostAnagrams = words[key].size();
+      mostAnagramKeys.clear();
+    }
+    mostAnagramKeys.push_back(key);
+  }
+}
+
+/**
+ * testFileExists
+ * 
+ * checks if the file exists in the directory
+ */
+inline bool testFileExists(const std::string &name)
+{
+  if (FILE *file = fopen(name.c_str(), "r"))
+  {
+    fclose(file);
+    return true;
+  }
+  else
+    return false;
+}
+
+/**
+ * main function
+ * 
+ * runs through validation
+ */
+int main(const int argc, char *const argv[])
+{
+  if (argc != 2)
+  {
+    std::cout << "Usage: ./anagramfinder <dictionary file>\n";
+    return -1;
+  }
+  const std::string fileName = argv[1];
+  if (!testFileExists(fileName))
+  {
+    std::cout << "Error: File '" << fileName << "' not found.\n";
+    return -1;
+  }
+  FindAnagrams(std::string(fileName)).printAnagrams();
+}
