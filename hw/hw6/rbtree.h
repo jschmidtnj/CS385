6c5e7cf53dead5e6e5a84f751f65c28797f5c7f4
Joshua <jschmidtnj@gmail.com>
2019-11-06 11:53:00 -0500

    added some recursive helper functions. still need to add insertion

--- hw/hw6/rbtree.h
+++ hw/hw6/rbtree.h
@@ -49,47 +49,47 @@ class RedBlackTreeIterator
 {
 public:
   /**
-     * Constructor
-     */
+   * Constructor
+   */
   RedBlackTreeIterator() : node_ptr{NULL}, tree{NULL} {}
 
   /**
-     * Equality operator. Compares node pointers.
-     */
+   * Equality operator. Compares node pointers.
+   */
   bool operator==(const RedBlackTreeIterator &rhs) const
   {
     return node_ptr == rhs.node_ptr;
   }
 
   /**
-     * Inequality operator. Compares node pointers.
-     */
+   * Inequality operator. Compares node pointers.
+   */
   bool operator!=(const RedBlackTreeIterator &rhs) const
   {
     return node_ptr != rhs.node_ptr;
   }
 
   /**
-     * Dereference operator. Returns a reference to the Node pointed to
-     * by node_ptr.
-     */
+   * Dereference operator. Returns a reference to the Node pointed to
+   * by node_ptr.
+   */
   Node<K, V> &operator*() const
   {
     return *node_ptr;
   }
 
   /**
-     * Dereference operator. Returns a pointer to the Node pointed to
-     * by node_ptr.
-     */
+   * Dereference operator. Returns a pointer to the Node pointed to
+   * by node_ptr.
+   */
   Node<K, V> *operator->() const
   {
     return node_ptr;
   }
 
   /**
-     * Preincrement operator. Moves forward to next larger value.
-     */
+   * Preincrement operator. Moves forward to next larger value.
+   */
   RedBlackTreeIterator &operator++()
   {
     Node<K, V> *p;
@@ -150,8 +150,8 @@ public:
   }
 
   /**
-     * Postincrement operator. Moves forward to next larger value.
-     */
+   * Postincrement operator. Moves forward to next larger value.
+   */
   RedBlackTreeIterator operator++(int)
   {
     RedBlackTreeIterator tmp(*this);
@@ -171,9 +171,9 @@ private:
   friend class RedBlackTree<K, V>;
 
   /**
-     * Constructor used to construct an iterator return value from a tree
-     * pointer.
-     */
+   * Constructor used to construct an iterator return value from a tree
+   * pointer.
+   */
   RedBlackTreeIterator(Node<K, V> *p, RedBlackTree<K, V> *t) : node_ptr(p), tree(t) {}
 };
 
@@ -184,31 +184,31 @@ public:
   typedef RedBlackTreeIterator<K, V> iterator;
 
   /**
-     * Constructor to create an empty red-black tree.
-     */
+   * Constructor to create an empty red-black tree.
+   */
   RedBlackTree() : root_{NULL}, size_{0} {}
 
   /**
-     * Constructor to create a red-black tree with the elements from the
-     * vector.
-     */
+   * Constructor to create a red-black tree with the elements from the
+   * vector.
+   */
   RedBlackTree(std::vector<std::pair<K, V>> &elements) : root_(NULL), size_(0)
   {
     insert_elements(elements);
   }
 
   /**
-     * Destructor.
-     */
+   * Destructor.
+   */
   ~RedBlackTree()
   {
     delete_tree(root_);
   }
 
   /**
-     * Inserts elements from the vector into the red-black tree.
-     * Duplicate elements are not inserted.
-     */
+   * Inserts elements from the vector into the red-black tree.
+   * Duplicate elements are not inserted.
+   */
   void insert_elements(std::vector<std::pair<K, V>> &elements)
   {
     for (size_t i = 0, len = elements.size(); i < len; ++i)
@@ -225,10 +225,10 @@ public:
   }
 
   /**
-     * Inserts a key-value pair into the red black tree.
-     * const iterator &it indicates where to start the search for the place to
-     * insert the node. If it == end(), the search starts at the root.
-     */
+   * Inserts a key-value pair into the red black tree.
+   * const iterator &it indicates where to start the search for the place to
+   * insert the node. If it == end(), the search starts at the root.
+   */
   void insert(const iterator &it, const std::pair<K, V> &key_value)
   {
     const K &key = key_value.first;
@@ -247,8 +247,8 @@ public:
   }
 
   /**
-     * Inserts a key-value pair into the red-black tree.
-     */
+   * Inserts a key-value pair into the red-black tree.
+   */
   void insert(const K &key, const V &value)
   {
     iterator e = end();
@@ -256,8 +256,8 @@ public:
   }
 
   /**
-     * Returns an ASCII representation of the red-black tree.
-     */
+   * Returns an ASCII representation of the red-black tree.
+   */
   std::string to_ascii_drawing()
   {
     BinaryTreePrinter<K, V> printer(root_);
@@ -265,49 +265,49 @@ public:
   }
 
   /**
-     * Returns the height of the red-black tree.
-     */
+   * Returns the height of the red-black tree.
+   */
   int height() const
   {
     return height(root_);
   }
 
   /**
-     * Returns the number of nodes in the red-black tree.
-     */
+   * Returns the number of nodes in the red-black tree.
+   */
   size_t size() const
   {
     return size_;
   }
 
   /**
-     * Returns the leaf count of the red-black tree.
-     */
+   * Returns the leaf count of the red-black tree.
+   */
   size_t leaf_count() const
   {
     return leaf_count(root_);
   }
 
   /**
-     * Returns the internal node count of the red-black tree.
-     */
+   * Returns the internal node count of the red-black tree.
+   */
   size_t internal_node_count() const
   {
     return internal_node_count(root_);
   }
 
   /**
-     * Returns the diameter of the red-black tree.
-     */
+   * Returns the diameter of the red-black tree.
+   */
   size_t diameter() const
   {
     return diameter(root_);
   }
 
   /**
-     * Returns the max width of the red-black tree, i.e. the largest number of
-     * nodes on any level.
-     */
+   * Returns the max width of the red-black tree, i.e. the largest number of
+   * nodes on any level.
+   */
   size_t max_width() const
   {
     size_t max_width = 0;
@@ -323,27 +323,27 @@ public:
   }
 
   /**
-     * Returns the successful search cost, i.e. the average number of nodes
-     * visited to find a key that is present.
-     */
+   * Returns the successful search cost, i.e. the average number of nodes
+   * visited to find a key that is present.
+   */
   double successful_search_cost() const
   {
     return size_ == 0 ? 0 : 1 + (double)sum_levels() / size_;
   }
 
   /**
-     * Returns the unsuccessful search cost, i.e. the average number of nodes
-     * visited to find a key that is not present.
-     */
+   * Returns the unsuccessful search cost, i.e. the average number of nodes
+   * visited to find a key that is not present.
+   */
   double unsuccessful_search_cost() const
   {
     return (double)sum_null_levels() / null_count();
   }
 
   /**
-     * Searches for item. If found, returns an iterator pointing
-     * at it in the tree; otherwise, returns end().
-     */
+   * Searches for item. If found, returns an iterator pointing
+   * at it in the tree; otherwise, returns end().
+   */
   iterator find(const K &key)
   {
     Node<K, V> *x = root_;
@@ -367,8 +367,8 @@ public:
   }
 
   /**
-     * Returns an iterator pointing to the first item in order.
-     */
+   * Returns an iterator pointing to the first item in order.
+   */
   iterator begin()
   {
     Node<K, V> *curr = root_;
@@ -417,16 +417,22 @@ private:
    */
   void insert_fixup(Node<K, V> *z)
   {
-    while (z->parent->color == RED) {
-      if (z->parent == z->parent->parent->left) {
+    while (z->parent->color == RED)
+    {
+      if (z->parent == z->parent->parent->left)
+      {
         Node<K, V> *y = z->parent->parent->right;
-        if (y->color == RED) {
+        if (y->color == RED)
+        {
           z->parent->color == BLACK;
           y->color = BLACK;
           z->parent->parent->color = RED;
           z = z->parent->parent;
-        } else {
-          if (z == z->parent->right) {
+        }
+        else
+        {
+          if (z == z->parent->right)
+          {
             z = z->parent;
             left_rotate(z);
           }
@@ -434,15 +440,21 @@ private:
           z->parent->parent->color = RED;
           right_rotate(z->parent->parent);
         }
-      } else {
+      }
+      else
+      {
         Node<K, V> *y = z->parent->parent->left;
-        if (y->color == RED) {
+        if (y->color == RED)
+        {
           z->parent->color == BLACK;
           y->color = BLACK;
           z->parent->parent->color = RED;
           z = z->parent->parent;
-        } else {
-          if (z == z->parent->left) {
+        }
+        else
+        {
+          if (z == z->parent->left)
+          {
             z = z->parent;
             right_rotate(z);
           }
@@ -456,67 +468,117 @@ private:
   }
 
   /**
-     * Left-rotate method described on p. 313 of CLRS.
-     */
+   * Left-rotate method described on p. 313 of CLRS.
+   */
   void left_rotate(Node<K, V> *x)
   {
-    // TODO
+    Node<K, V> *y = x->right;
+    x->right = y->left;
+    if (y->left != NULL)
+      y->left->parent = x;
+    y->parent = x->parent;
+    if (x->parent == NULL)
+      root_ = y;
+    else if (x == x->parent->left)
+      x->parent->left = y;
+    else
+      x->parent->right = y;
+    y->left = x;
+    x->parent = y;
   }
 
   /**
-     * Right-rotate method described on p. 313 of CLRS.
-     */
+   * Right-rotate method described on p. 313 of CLRS.
+   */
   void right_rotate(Node<K, V> *x)
   {
-    // TODO
+    Node<K, V> *y = x->left;
+    x->left = y->right;
+    if (y->right != NULL)
+      y->right->parent = x;
+    y->parent = x->parent;
+    if (x->parent == NULL)
+      root_ = y;
+    else if (x == x->parent->right)
+      x->parent->right = y;
+    else
+      x->parent->left = y;
+    y->right = x;
+    x->parent = y;
   }
 
   /**
-     * Returns the height of the red-black tree starting at node.
-     * A null node starts at height -1.
-     */
+   * Returns the height of the red-black tree starting at node.
+   * A null node starts at height -1.
+   */
   int height(Node<K, V> *node) const
   {
-    // TODO
+    if (node == NULL)
+      return -1;
+    return 1 + max(height(node->left), height(node->right));
   }
 
   /**
-     * Returns the count of leaves in the red-black tree starting at node.
-     * For this method, a leaf is a non-null node that has no children.
-     */
+   * Returns the count of leaves in the red-black tree starting at node.
+   * For this method, a leaf is a non-null node that has no children.
+   */
   size_t leaf_count(Node<K, V> *node) const
   {
-    // TODO
+    if (node == NULL)
+      return 0;
+    if (node->left == NULL && node->right == NULL)
+      return 1;
+    size_t res = 0;
+    res += leaf_count(node->left);
+    res += leaf_count(node->right);
+    return res;
   }
 
   /**
-     * Returns the count of internal nodes in the red-black tree starting at
-     * node.
-     * An internal node has at least one child.
-     */
+   * Returns the count of internal nodes in the red-black tree starting at
+   * node.
+   * An internal node has at least one child.
+   */
   size_t internal_node_count(Node<K, V> *node) const
   {
-    // TODO
+    if (node == NULL || (node->left == NULL && node->right == NULL))
+      return 0;
+    size_t res = 1;
+    res += leaf_count(node->left);
+    res += leaf_count(node->right);
+    return res;
   }
 
   /**
-     * Returns the diameter of the red-black tree rooted at node.
-     * Diameter is defined as the number of nodes on the longest path between
-     * two (non-null) leaves in the tree. The path does not necessarily have to
-     * pass through the root.
-     */
+   * Returns the diameter of the red-black tree rooted at node.
+   * Diameter is defined as the number of nodes on the longest path between
+   * two (non-null) leaves in the tree. The path does not necessarily have to
+   * pass through the root.
+   */
   int diameter(Node<K, V> *node) const
   {
-    // TODO
+    if (node == NULL)
+      return 0;
+    int left_diameter = diameter(node->left),
+        right_diameter = diameter(node->right),
+        left_height = height(node->left),
+        right_height = height(node->right),
+        current_diameter = left_height + right_height + 1,
+        max_diameter = max(left_diameter, right_diameter);
+    return max(current_diameter, max_diameter);
   }
 
   /**
-     * Returns the width of the red-black tree at the designated level.
-     * Width is defined as the number of nodes residing at a level.
-     */
+   * Returns the width of the red-black tree at the designated level.
+   * Width is defined as the number of nodes residing at a level.
+   */
   size_t width(Node<K, V> *node, size_t level) const
   {
-    // TODO
+    if (node == NULL)
+      return 0;
+    if (level == 1)
+      return 1;
+    return width(node->left, level - 1) + width(node->right, level - 1);
   }
 
   size_t null_count() const
@@ -525,11 +587,16 @@ private:
   }
 
   /**
-     * Returns the count of null nodes in the red-black tree starting at node.
-     */
+   * Returns the count of null nodes in the red-black tree starting at node.
+   */
   size_t null_count(Node<K, V> *node) const
   {
-    // TODO
+    if (node == NULL)
+      return 1;
+    size_t res = 0;
+    res += leaf_count(node->left);
+    res += leaf_count(node->right);
+    return res;
   }
 
   size_t sum_levels() const
@@ -538,19 +605,21 @@ private:
   }
 
   /**
-     * Returns the sum of the levels of each non-null node in the red-black
-     * tree starting at node.
-     * For example, the tree
-     *   5 <- level 0
-     *  / \
-     * 2   8 <- level 1
-     *      \
-     *       10 <- level 2
-     * has sum 0 + 2(1) + 2 = 4.
-     */
+   * Returns the sum of the levels of each non-null node in the red-black
+   * tree starting at node.
+   * For example, the tree
+   *   5 <- level 0
+   *  / \
+   * 2   8 <- level 1
+   *      \
+   *       10 <- level 2
+   * has sum 0 + 2(1) + 2 = 4.
+   */
   size_t sum_levels(Node<K, V> *node, size_t level) const
   {
-    // TODO
+    if (node == NULL)
+      return 0;
+    return level + sum_levels(node->left, level + 1) + sum_levels(node->right, level + 1);
   }
 
   size_t sum_null_levels() const
@@ -559,21 +628,23 @@ private:
   }
 
   /**
-     * Returns the sum of the levels of each null node in the red-black tree
-     * starting at node.
-     * For example, the tree
-     *    5 <- level 0
-     *   / \
-     *  2   8 <- level 1
-     * / \ / \
-     * * * * 10 <- level 2
-     *       / \
-     *       * * <- level 3
-     * has sum 3(2) + 2(3) = 12.
-     */
+   * Returns the sum of the levels of each null node in the red-black tree
+   * starting at node.
+   * For example, the tree
+   *    5 <- level 0
+   *   / \
+   *  2   8 <- level 1
+   * / \ / \
+   * * * * 10 <- level 2
+   *       / \
+   *     * * <- level 3
+   * has sum 3(2) + 2(3) = 12.
+   */
   size_t sum_null_levels(Node<K, V> *node, size_t level) const
   {
-    // TODO
+    if (node == NULL)
+      return level;
+    return sum_levels(node->left, level + 1) + sum_levels(node->right, level + 1);
   }
 };
 
