3615fa43e5c2d134306b71ea26aa2e021c2608da
Joshua <jschmidtnj@gmail.com>
2019-11-18 14:34:54 -0500

    got everything for homework 7 working finally

--- hw/hw7/shortestpaths.cpp
+++ hw/hw7/shortestpaths.cpp
@@ -8,6 +8,13 @@
 #include <utility>
 #include <bits/stdc++.h>
 
+/**
+ * ShortestPaths
+ * 
+ * class declaration for shortest paths
+ * object used to get shortest paths between
+ * 2 elements using floyd's algorithm
+ */
 class ShortestPaths
 {
 public:
@@ -24,8 +31,10 @@ private:
   {
     Node *left;
     Node *right;
-    int x, y;
-    Node(int x_val, int y_val)
+    unsigned long *x;
+    unsigned long *y;
+    bool assignedChildren = false;
+    Node(unsigned long *x_val, unsigned long *y_val)
     {
       x = x_val;
       y = y_val;
@@ -33,19 +42,20 @@ private:
   };
   std::string matrixFilename;
   unsigned int numNodes = 0;
-  unsigned int maxDistance = 0;
-  unsigned int maxPath = 0;
+  unsigned long maxDistance = 0;
+  unsigned long maxPath = 0;
   bool deleteArrays = false;
-  unsigned int **distanceMatrix;
-  unsigned int **pathMatrix;
-  unsigned int **intermediateMatrix;
+  unsigned long **distanceMatrix;
+  unsigned long **pathMatrix;
+  unsigned long **intermediateMatrix;
   unsigned int numDigits(unsigned int num);
-  void printMatrix(unsigned int maxVal, unsigned int **array, bool chars);
+  void printMatrix(unsigned long maxVal, unsigned long **array, bool chars);
 };
 
 /**
- * numDigits gets the number of digits
- * in a given int
+ * numDigits
+ * gets the number of digits
+ * in a given number
  */
 unsigned int ShortestPaths::numDigits(unsigned int num)
 {
@@ -57,100 +67,128 @@ unsigned int ShortestPaths::numDigits(unsigned int num)
   return count;
 }
 
+/**
+ * printPaths
+ * 
+ * prints the paths in arrow notation
+ * to get between any 2 points
+ */
+
 void ShortestPaths::printPaths()
 {
-  for (unsigned int i = 0; i < numNodes; i++)
+  for (unsigned long i = 0; i < numNodes; i++)
   {
-    for (unsigned int j = 0; j < numNodes; j++)
+    for (unsigned long j = 0; j < numNodes; j++)
     {
       std::cout << char('A' + i) + std::string(" -> ") + char('A' + j) + ", distance: ";
-      if (pathMatrix[i][j] == UINT_MAX)
+      if (pathMatrix[i][j] == ULONG_MAX)
       {
         std::cout << "infinity, path: none\n";
         continue;
       }
       std::cout << pathMatrix[i][j] << ", path: ";
       // print the path here
-      std::vector<unsigned int> path;
-      Node *root = new Node(i, j);
-      Node *current = root;
-      std::stack<Node *> nodeStack;
-      std::unordered_set<unsigned int> pathSet;
-      pathSet.reserve(numNodes * 2);
-      while (current != nullptr || !nodeStack.empty())
+      std::vector<unsigned long> path;
+      if (i == j)
       {
-        while (current != nullptr)
-        {
-          unsigned int val = intermediateMatrix[current->x][current->y];
-          if (val == UINT_MAX)
-          {
-            current->left = nullptr;
-            current->right = nullptr;
-          }
-          else
-          {
-            current->left = new Node(current->x, val);
-            current->right = new Node(val, current->y);
-          }
-          nodeStack.push(current);
-          current = current->left;
-        }
-        current = nodeStack.top();
-        nodeStack.pop();
-        if (pathSet.find(current->x) == pathSet.end())
-        {
-          path.push_back(current->x);
-          pathSet.insert(current->x);
-        }
-        if (pathSet.find(current->y) == pathSet.end())
-        {
-          path.push_back(current->y);
-          pathSet.insert(current->y);
-        }
-        current = current->right;
+        path.push_back(i);
       }
-      // delete tree
-      std::queue<Node *> nodeQueue;
-      nodeQueue.push(root);
-      Node *front;
-      while (!nodeQueue.empty())
+      else
       {
-        front = nodeQueue.front();
-        nodeQueue.pop();
-        if (front->left)
-          nodeQueue.push(front->left);
-        if (front->right)
-          nodeQueue.push(front->right);
-        delete front;
+        Node *root = new Node(&i, &j);
+        Node *current = root;
+        std::stack<Node *> nodeStack;
+        nodeStack.push(current);
+        while (!nodeStack.empty())
+        {
+          current = nodeStack.top();
+          nodeStack.pop();
+          if (!current->assignedChildren)
+          {
+            unsigned long *val = &intermediateMatrix[*current->x][*current->y];
+            if (*val == ULONG_MAX)
+            {
+              current->left = nullptr;
+              current->right = nullptr;
+            }
+            else
+            {
+              current->left = new Node(current->x, val);
+              current->right = new Node(val, current->y);
+            }
+            current->assignedChildren = true;
+          }
+          if (current->right != nullptr)
+            nodeStack.push(current->right);
+          if (current->left != nullptr)
+            nodeStack.push(current->left);
+          if (current->left == nullptr && current->right == nullptr)
+            path.push_back(*current->x);
+        }
+        path.push_back(j);
+        // delete tree
+        std::queue<Node *> nodeQueue;
+        nodeQueue.push(root);
+        Node *front;
+        while (!nodeQueue.empty())
+        {
+          front = nodeQueue.front();
+          nodeQueue.pop();
+          if (front->left)
+            nodeQueue.push(front->left);
+          if (front->right)
+            nodeQueue.push(front->right);
+          delete front;
+        }
       }
       std::cout << char('A' + path[0]);
       for (unsigned long k = 1; k < path.size(); k++)
         std::cout << " -> " << char('A' + path[k]);
-      std::cout << '\n';
+      if (i != numNodes - 1 || j != numNodes - 1)
+        std::cout << '\n';
     }
   }
-  std::cout << '\n';
 }
 
+/**
+ * printIntermediate
+ * 
+ * prints the intermediate matrix
+ */
 void ShortestPaths::printIntermediate()
 {
   std::cout << "Intermediate vertices:\n";
   printMatrix(1, intermediateMatrix, true);
 }
 
+/**
+ * printPathLengths
+ * 
+ * prints the max path lengths matrix
+ */
 void ShortestPaths::printPathLengths()
 {
   std::cout << "Path lengths:\n";
   printMatrix(maxPath, pathMatrix, false);
 }
 
+/**
+ * printDistanceMatrix
+ * 
+ * public method for printing the distance matrix
+ */
 void ShortestPaths::printDistanceMatrix()
 {
   std::cout << "Distance matrix:\n";
   printMatrix(maxDistance, distanceMatrix, false);
 }
 
-void ShortestPaths::printMatrix(unsigned int maxVal, unsigned int **array, bool chars)
+/**
+ * printMatrix
+ * 
+ * prints matrix values
+ */
+void ShortestPaths::printMatrix(unsigned long maxVal, unsigned long **array, bool chars)
 {
   unsigned int maxDigits = numDigits(maxVal);
   for (unsigned int i = 0; i < maxDigits; i++)
@@ -168,10 +206,10 @@ void ShortestPaths::printMatrix(unsigned int maxVal, unsigned int **array, bool 
     std::cout << char('A' + i);
     for (unsigned int j = 0; j < numNodes; j++)
     {
-      unsigned int numSpaces = array[i][j] == UINT_MAX ? maxDigits : maxDigits - numDigits(array[i][j]) + 1;
+      unsigned int numSpaces = array[i][j] == ULONG_MAX ? maxDigits : maxDigits - numDigits(array[i][j]) + 1;
       for (unsigned int k = 0; k < numSpaces; k++)
         std::cout << ' ';
-      if (array[i][j] == UINT_MAX)
+      if (array[i][j] == ULONG_MAX)
         std::cout << "-";
       else
       {
@@ -186,6 +224,11 @@ void ShortestPaths::printMatrix(unsigned int maxVal, unsigned int **array, bool 
   std::cout << '\n';
 }
 
+/**
+ * destructor
+ * 
+ * ShortestPaths destructor for deleting arrays on the heap
+ */
 ShortestPaths::~ShortestPaths()
 {
   if (deleteArrays)
@@ -202,6 +245,12 @@ ShortestPaths::~ShortestPaths()
   }
 }
 
+/**
+ * processFile
+ * 
+ * uses the file string to process the file and
+ * get the shortest path data in the necessary arrays
+ */
 void ShortestPaths::processFile()
 {
   std::ifstream pathFile(matrixFilename);
@@ -214,22 +263,22 @@ void ShortestPaths::processFile()
     throw std::invalid_argument("Error: Invalid number of vertices '" + line + "' on line 1.");
   iss.clear();
   numNodes = potentialNumNodes;
-  distanceMatrix = new unsigned int *[numNodes];
-  pathMatrix = new unsigned int *[numNodes];
-  intermediateMatrix = new unsigned int *[numNodes];
+  distanceMatrix = new unsigned long *[numNodes];
+  pathMatrix = new unsigned long *[numNodes];
+  intermediateMatrix = new unsigned long *[numNodes];
   for (unsigned int i = 0; i < numNodes; i++)
   {
-    distanceMatrix[i] = new unsigned int[numNodes];
-    pathMatrix[i] = new unsigned int[numNodes];
-    intermediateMatrix[i] = new unsigned int[numNodes];
+    distanceMatrix[i] = new unsigned long[numNodes];
+    pathMatrix[i] = new unsigned long[numNodes];
+    intermediateMatrix[i] = new unsigned long[numNodes];
     for (unsigned int j = 0; j < numNodes; j++)
     {
       if (i == j)
         distanceMatrix[i][j] = 0;
       else
-        distanceMatrix[i][j] = UINT_MAX;
+        distanceMatrix[i][j] = ULONG_MAX;
       pathMatrix[i][j] = distanceMatrix[i][j];
-      intermediateMatrix[i][j] = UINT_MAX;
+      intermediateMatrix[i][j] = ULONG_MAX;
     }
   }
   deleteArrays = true;
@@ -255,7 +304,7 @@ void ShortestPaths::processFile()
     if (!(iss >> potentialEdgeWeight) || potentialEdgeWeight <= 0)
       throw std::invalid_argument("Error: Invalid edge weight '" + splitLine[2] + "' on line " + std::to_string(lineNum) + ".");
     iss.clear();
-    unsigned int edgeWeight = potentialEdgeWeight;
+    unsigned long edgeWeight = potentialEdgeWeight;
     if (edgeWeight > maxDistance)
       maxDistance = edgeWeight;
     distanceMatrix[splitLine[0][0] - 'A'][splitLine[1][0] - 'A'] = edgeWeight;
@@ -266,9 +315,11 @@ void ShortestPaths::processFile()
     for (unsigned int i = 0; i < numNodes; i++)
       for (unsigned int j = 0; j < numNodes; j++)
       {
-        if (pathMatrix[i][k] == UINT_MAX || pathMatrix[k][j] == UINT_MAX)
+        if (pathMatrix[i][k] == ULONG_MAX || pathMatrix[k][j] == ULONG_MAX)
           continue;
-        unsigned int potentialMin = pathMatrix[i][k] + pathMatrix[k][j];
+        if (pathMatrix[i][k] > pathMatrix[i][j] || pathMatrix[k][j] > pathMatrix[i][j])
+          continue;
+        unsigned long potentialMin = pathMatrix[i][k] + pathMatrix[k][j];
         if (potentialMin < pathMatrix[i][j])
         {
           pathMatrix[i][j] = potentialMin;
@@ -277,10 +328,14 @@ void ShortestPaths::processFile()
       }
   for (unsigned int i = 0; i < numNodes; i++)
     for (unsigned int j = 0; j < numNodes; j++)
-      if (pathMatrix[i][j] != UINT_MAX && pathMatrix[i][j] > maxPath)
+      if (pathMatrix[i][j] != ULONG_MAX && pathMatrix[i][j] > maxPath)
         maxPath = pathMatrix[i][j];
 }
 
+/**
+ * simple constructor used just for setting
+ * filename for shortestpaths finding object
+ */
 ShortestPaths::ShortestPaths(const std::string fileName)
 {
   matrixFilename = fileName;
@@ -302,6 +357,10 @@ inline bool testFileExists(const std::string &name)
     return false;
 }
 
+/**
+ * main function with initial argument
+ * insertion logic
+ */
 int main(const int argc, char *const argv[])
 {
   if (argc != 2)
---
+++ notes/spanning_trees/README.md
@@ -0,0 +1,33 @@
+# spanning trees
+
+A **spanning tree** of an undirected connected graph is its connected acyclic subgraph (a tree) that contains all the vertices of the graph. If such a graph has weights assigned to its edges, a minimum spanning tree is its spanning tree of the smallest weight, where the weight of a tree is defined as the sum of the weights on all its edges.
+
+## prim's algorithm
+
+greedy algorithm
+
+tree vertices | remaining vertices
+a(-,-) | b(a, 3), c(-, inf), d(-, inf), e(0, 6), f(a, 3)
+b(a, 3) | c(b, 1), d(-, inf), e(a, 6), f(b, 4)
+c(b, 1) | d(c, 6), e(a, 6), f(b, 4)
+f(b, 4) | d(f, 5), e(f, 2)
+e(f, 2) | d(f, 5)
+d(f, 5)
+
+- running time for adjacency matrix + unordered array priority queue is $\theta(V)$, where V is the number of vertices
+- adjacency list + min heap priority queue is faster
+  - deletion, insertion, decrease-key is $O(logV)$
+  - $|V|$ for building the loop
+  - $|V| - 1$ for deletion of the smallest element
+  - $|E|$ comparisons - sum of the lengths of all adjacency list ($2 \cdot |V|$)
+  - this results in $(E + V) \cdot log(V)$
+  - $V-1 <= E$
+  - $E \cdot log V$
+- can also use Kruskal's algorithm
+
+## union find (part of Kruskal's)
+
+- makeset(x) - requires the creation of a single-node tree. theta(1) * n = theta(n)
+- union(x, y) - attaches the root of y's subtree to the root of x's subtree - theta(1)
+- find(x) - follow the pointer chain from x to the root - O(n)
+- no cycle means find(x) != find(y)
